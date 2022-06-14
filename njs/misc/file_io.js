41ceed5ab430a63d473be734e875c9317567d934
Sergey A. Osokin <osa@FreeBSD.org.ru>
2022-06-13 21:12:40 -0400

    Convert tabs to spaces

--- njs/misc/file_io.js
+++ njs/misc/file_io.js
@@ -2,23 +2,23 @@ var fs = require('fs');
 var STORAGE = "/tmp/njs_storage"
 
 function push(r) {
-	fs.appendFileSync(STORAGE, r.requestBody);
-	r.return(200);
+    fs.appendFileSync(STORAGE, r.requestBody);
+    r.return(200);
 }
 
 function flush(r) {
-	fs.writeFileSync(STORAGE, "");
-	r.return(200);
+    fs.writeFileSync(STORAGE, "");
+    r.return(200);
 }
 
 function read(r) {
-	var data = "";
-	try {
-		data = fs.readFileSync(STORAGE);
-	} catch (e) {
-	}
+    var data = "";
+    try {
+        data = fs.readFileSync(STORAGE);
+    } catch (e) {
+    }
 
-	r.return(200, data);
+    r.return(200, data);
 }
 
 export default {push, flush, read};
--- njs/stream/detect_http.js
+++ njs/stream/detect_http.js
@@ -1,20 +1,20 @@
 var is_http = 0;
 
 function detect_http(s) {
-	s.on('upload', function (data, flags) {
-		var n = data.indexOf('\r\n');
-		if (n != -1 && data.substr(0, n - 1).endsWith(" HTTP/1.")) {
-			is_http = 1;
-		}
+    s.on('upload', function (data, flags) {
+        var n = data.indexOf('\r\n');
+        if (n != -1 && data.substr(0, n - 1).endsWith(" HTTP/1.")) {
+            is_http = 1;
+        }
 
-		if (data.length || flags.last) {
-			s.done();
-		}
-	});
+        if (data.length || flags.last) {
+            s.done();
+        }
+    });
 }
 
 function upstream_type(s) {
-	return is_http ? "httpback" : "tcpback";
+    return is_http ? "httpback" : "tcpback";
 }
 
 export default {detect_http, upstream_type}
