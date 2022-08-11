84b0d646d0b6f535f31189e16981e44c9f842864
Dmitry Volyntsev <xeioexception@gmail.com>
2022-08-10 20:25:42 -0700

    [enh] rewriting stream/auth_request with an async callback

--- njs/stream/auth_request.js
+++ njs/stream/auth_request.js
@@ -1,14 +1,17 @@
 function preread_verify(s) {
     var collect = '';
 
-    s.on('upload', function (data, flags) {
+    s.on('upload', async function (data, flags) {
         collect += data;
 
         if (collect.length >= 5 && collect.startsWith('MAGiK')) {
             s.off('upload');
-            ngx.fetch('http://127.0.0.1:8080/validate',
-                      {body: collect.slice(5,7), headers: {Host:'aaa'}})
-            .then(reply => (reply.status == 200) ? s.done(): s.deny())
+
+            let reply = await ngx.fetch('http://127.0.0.1:8080/validate',
+                                        {body: collect.slice(5,7),
+                                         headers: {Host:'aaa'}});
+
+            (reply.status == 200) ? s.done(): s.deny();
 
         } else if (collect.length) {
             s.deny();
